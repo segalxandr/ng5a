04e040d4f55d6e296307917dde3f06baed163dd8
Alex Segal <asegal@cfl.rr.com>
2017-12-31 20:51:24 -0500

    Inerpolation/props segment

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  itemCount: number = 4;
+  btnText: string = 'Add an Item';
+  constructor() {}
+
+  ngOnInit() {}
+}
